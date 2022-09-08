9f29b1ec3c5f6ebae04cf1e3f9c580038afad01c
CaroDGomez <caro5952@gmail.com>
2022-09-08 15:43:30 -0500

    Crea la ruta /products/new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/products', to: 'products#index'
-
+  get '/products/new', to: 'products#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
